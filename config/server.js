a894587cb000cc1615d0bab3889443b8b4658098
nilsweichler <51708114+nilsweichler@users.noreply.github.com>
2021-09-25 02:17:56 +0200

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '83047a04b36863945f58909756c43a2e'),
+    },
+  },
+});
